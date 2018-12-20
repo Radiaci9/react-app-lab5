bc191c6505943189932761e47de5027da2b5b34c
Radiaci9 <cvirko1998@mail.ru>
2018-12-20 15:35:04 +0300

    Updates

---
+++ precache-manifest.42bf28e79fc1f55a5c18d3af5e06b0a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54f8f4fb8d724a875c2d",
+    "url": "/lab5/static/js/runtime~main.54f8f4fb.js"
+  },
+  {
+    "revision": "0c2e4a6e2ca1906bf706",
+    "url": "/lab5/static/js/main.0c2e4a6e.chunk.js"
+  },
+  {
+    "revision": "fff05e59e4be81286ca5",
+    "url": "/lab5/static/js/1.fff05e59.chunk.js"
+  },
+  {
+    "revision": "0c2e4a6e2ca1906bf706",
+    "url": "/lab5/static/css/main.7e34a6ff.chunk.css"
+  },
+  {
+    "revision": "fff05e59e4be81286ca5",
+    "url": "/lab5/static/css/1.bf3a06c0.chunk.css"
+  },
+  {
+    "revision": "259a45cd4b566402692b1af56569e58f",
+    "url": "/lab5/index.html"
+  }
+];
